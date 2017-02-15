20765bcfc8361d3fdd9d01efc0e5effbf4055ee5
nsegen <segennikita@gmail.com>
2017-02-15 14:58:49 +0300

    vertex coating

--- MethodsOfAlgorithm/vertex_coating/vertex_coating.py
+++ MethodsOfAlgorithm/vertex_coating/vertex_coating.py
@@ -2,6 +2,7 @@ from find_coating_variants.full_search import full_search
 from find_coating_variants.greedy_algorithm import greedy_algorithm
 from find_coating_variants.lazy_algorithm import lazy_algorithm
 import copy
+import time
 
 graph = {}
 edge = 1
@@ -10,8 +11,12 @@ for i in open('input').readlines():
     edge += 1
 print(graph)
 
+start = time.time()
 print(full_search(graph))
-
+print(time.time() - start)
+start = time.time()
 print(greedy_algorithm(copy.deepcopy(graph)))
-
+print(time.time() - start)
+start = time.time()
 print(lazy_algorithm(copy.deepcopy(graph)))
+print(time.time() - start)
